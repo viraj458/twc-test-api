0ff4526236fa04477ace4fe03d4e194bba40c6d6
Viraj <virajmalaka94@gmail.com>
2023-03-02 19:03:29 +0530

    created createContact function in contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,7 +2,7 @@ const Contact = require('../models/contactModel')
 const mongoose = require('mongoose')
 
 //get all contacts
-const getContacts = async() => {
+const getContacts = async(req, res) => {
     try {
         const user_id = req.user._id
         const contacts = await Contact.find({user_id}).sort({createdAt: -1})
@@ -14,7 +14,7 @@ const getContacts = async() => {
 }
 
 //get a contact
-const getContact = async() => {
+const getContact = async(req, res) => {
     try {
         const {id} = req.body
         if(!mongoose.Types.ObjectId.isValid(id)){
@@ -29,4 +29,15 @@ const getContact = async() => {
         res.status(400).json({error:err.message})
     }
 
+}
+
+//create a contact
+const createContact = async(req, res) => {
+    try {
+        const {full_name,gender,email,phone_number,user_id } = req.body
+        const contact = await Contact.create({full_name,gender,email,phone_number,user_id})
+        res.status(200).json(contact)
+    } catch (err) {
+        res.status(400).json({error:err.message})
+    }
 }

7d2ccd426c4119d9a10b9bc6ba3e41c862866459
Viraj <virajmalaka94@gmail.com>
2023-03-03 03:59:46 +0530

    created updateContact function in contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -58,3 +58,31 @@ const deleteContact = async(req, res) => {
         res.status(400).json({error:err.message})
     }
 }
+
+//update a contact
+const updateContact = async(req, res) => {
+    try {
+        const {id} = req.params
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({error: "contact not found"})
+        }
+        const contact = await Contact.findByIdAndUpdate({id}, {
+            ...req.body
+        })
+        if(!contact){
+            res.status(404).json({error:"contact not found"})
+        }
+        res.status(200).json(contact)
+
+    } catch (err) {
+        res.status(400).json({error:err.message})
+    }
+}
+
+module.exports = {
+    getContacts,
+    getContact,
+    createContact,
+    deleteContact,
+    updateContact
+}
